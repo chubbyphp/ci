0521bbee07acbdfb0821e68d90aaaf57b58a6ffd
Dominik Zogg <dominik.zogg@hotmail.com>
2021-02-14 21:32:10 +0100

    output step

--- php80/entrypoint.sh
+++ php80/entrypoint.sh
@@ -2,6 +2,9 @@
 set -e
 
 php -v
+echo "composer update ${COMPOSER_ARGS}"
 composer update $COMPOSER_ARGS
+echo "composer test"
 composer test
+echo "vendor/bin/php-coveralls --coverage_clover=build/phpunit/clover.xml --json_path=build/coveralls.json -v"
 vendor/bin/php-coveralls --coverage_clover=build/phpunit/clover.xml --json_path=build/coveralls.json -v
